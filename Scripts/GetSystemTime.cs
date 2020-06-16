87e60b53d63ff4c08d6b7d9882766e8b43c9aff1
Andrew Morse <contactandrewmorse@gmail.com>
2020-06-15 22:19:00 -0400

    Add files via upload
    Updating system time script which will act as a utility for the dynamic day/night cycle.

---
+++ Scripts/GetSystemTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Note: The following class gets tthe user's current system time. - Amorse
+public class GetSystemTime : MonoBehaviour
+{
+	private int m_Hour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (null != m_Hour) 
+        {
+            m_Hour = System.DateTime.Now.Hour;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PrintHour(m_Hour);
+        
+    }
+
+    void PrintHour(int currentHour) 
+    {
+    	Debug.Log("The current hour is " + m_Hour);
+    }
+}
